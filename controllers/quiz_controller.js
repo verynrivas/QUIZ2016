59e8c223f930ab0077c261bdc67e17755800a6a1
verynrivas <veronica.rivas.boscan@alumnos.upm.es>
2016-04-27 00:26:11 +0200

    Primera pregunta sin perder respuesta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,12 +1,13 @@
 //Get /question
 
 export.question = function (req, res, next) {
-	res.render('quizzes/question', {question: "Capital de Itaia"});
+	var answer = req.query.answer || '';
+	res.render('quizzes/question', {question: "Capital de Italia", answer: answer});
 
 };
 
 //GET /check
 export.check = function(req,res, next) {
 	var result = req.query.answer === 'Roma' ? 'Correcta' : 'Incorrecta';
-	res.render('quizzes/result', {result: result});
+	res.render('quizzes/result', {result: result, answer: answer});
 };
