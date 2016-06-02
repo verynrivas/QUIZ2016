8e0bceb12691d5271b0ca820f97a2810e2592215
verynrivas <veronica.rivas.boscan@alumnos.upm.es>
2016-06-02 17:32:40 +0200

    Migracion en local

--- models/index.js
+++ models/index.js
@@ -22,7 +22,7 @@ var sequelize = new Sequelize(url, {storage: storage, omitNull: true});
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().then(function() { //sync() crea la tabla quiz
+/*sequelize.sync().then(function() { //sync() crea la tabla quiz
 	return Quiz.count().then(function(c) {
 			if (c===0) { //la tabla se inicializa si está vacía
 				return Quiz.bulkCreate([ {question: 'Capital de Italia', answer: 'Roma'},
@@ -34,6 +34,6 @@ sequelize.sync().then(function() { //sync() crea la tabla quiz
 }).catch(function(error) {
 	console.log("Error Sincronizando las tablas de la BBDD:",error);
 	process.exit(1);
-});
+});*/
 
 exports.Quiz = Quiz; //exportar definicion de tabla Quiz
